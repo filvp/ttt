8e0ca0eba169366966dc4805cf69880f05482c3b
verapavlovnamsc <filatovaverapavlovna@gmail.com>
2022-07-19 04:16:30 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+    <Header />
+
+    <nav>
+      <router-link to="/about">Каталог</router-link> >
+      <router-link to="/about">Строительные материалы</router-link> >
+      <router-link to="/about">Сухие строительные смеси</router-link> >
+      <router-link to="/about">Шпаклевки</router-link> >
+      <router-link to="/">Шпаклевка</router-link>
+    </nav>
+
+    <router-view/>
+  </div>
 </template>
+<script>
+
+
+
+import Header from "@/components/Header"
+export default {
+  components: {
+
+    Header
+  }
+}
+</script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@200;300;400;500;600;700;800;900&family=Open+Sans:wght@300;400;500;700;800&display=swap');
+body{
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -16,7 +40,7 @@
 }
 
 nav {
-  padding: 30px;
+  //padding: 30px;
 
   a {
     font-weight: bold;
@@ -26,5 +50,38 @@ nav {
       color: #42b983;
     }
   }
+}
+.wrapper{
+  margin: 0 auto;
+}
+.vueperslides__track{
+  height: 70px;
+}
+.vueperslide__content-wrapper{
+  height: 70px;
+}
+.vueperslide vueperslide--visible{
+  height: 70px;
+}
+.vueperslide vueperslide--active vueperslide--visible{
+  height: 70px;
+}
+.vueperslides__track-inner{
+  height: 70px;
+}
+.vueperslides__parallax-wrapper{
+  height: 70px;
+}
+.vueperslides__inner{
+  height: 70px;
+}
+.vueperslides{
+  height: 70px;
+}
+.vueperslide__content-wrapper{
+  height: 70px !important;
+}
+.vueperslides__arrow button{
+  width: 50%;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <CardItem />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import CardItem from "@/components/Card-item"
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    CardItem
   }
 }
 </script>
